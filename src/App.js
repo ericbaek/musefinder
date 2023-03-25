499ead2f09e3ca063b7bde55cd7c2fa09523069d
heart <66947260+bampudding@users.noreply.github.com>
2023-03-25 13:31:35 +0900

    Design

--- src/App.js
+++ src/App.js
@@ -9,15 +9,18 @@ import Sortset from './DesignSystem/Component/In-Content/Sortset';
 
 function App() {
 
+  /* Card */
+  /* Right Accent 미사용 시 { NoAccent } 처리 */
   var NoAccent = { display : 'none' }
-  var BGLocationNear = { background : 'hsl(213, 100%, 56%)', color : 'white' }
-  var BGLocationMedium = { background : 'hsl(39, 85%, 53%)', color : 'white' }
-  var BGLocationFar = { background : 'hsl(0, 79%, 56%)', color : 'white' }
+  /* 저장한 장소 및 근처 오락실까지의 거리를 기준으로 한 배경색 */
+  var BGLocationNear = { background : 'hsl(213, 100%, 56%)', color : 'white' } /* < 5km */
+  var BGLocationMedium = { background : 'hsl(39, 85%, 53%)', color : 'white' } /* < 10km */
+  var BGLocationFar = { background : 'hsl(0, 79%, 56%)', color : 'white' } /* > 10km */
 
   return (
-    <div className="System">
+    <div className="System"> { /* 시스템 전반 Flex / Column / Padding / Gap */ }
       
-      <div className="DesktopSearch">
+      <div className="DesktopSearch"> { /* 데스크톱 환경에서의 검색창 + 메인 액티비티 */ }
         <div className="SearchTop">
           <Search Icon="" Placeholder="검색" Sort=""></Search>
           <div className="DesktopSearch-Sortset">
@@ -27,20 +30,22 @@ function App() {
             <Sortset Text="팝픈뮤직"></Sortset>
           </div>
         </div>
-        <div className="MainActivity">
+        <div className="MainActivity"> { /* 메인 액티비티 */ }
           <div className="Group">
             <ContentTitle Title="저장한 장소" Paragraph="관리"></ContentTitle>
-            <div className="Row">
-              <Card Title="펀시티 건대점" Paragraph="3.2km" AccentText="" BGAccent={ BGLocationMedium } Accent={ NoAccent }></Card>
-              <Card Title="노원 노리존" Paragraph="4.8km" AccentText="" BGAccent={ BGLocationFar }  Accent={ NoAccent }></Card>
+            <div className="Row Gap-8">
+              <Card Title="펀시티 건대점" Paragraph="1.4km" AccentText="" Accent={ NoAccent }></Card>
+              <Card Title="노원 노리존" Paragraph="4.8km" AccentText="" Accent={ NoAccent }></Card>
             </div>
           </div>
-          <div className="Group">
+          <div className="Group"> { /* 그룹 내 Flex / Column / Gap */ }
             <ContentTitle Title="근처 오락실" Paragraph=""></ContentTitle>
-            <div className="Row">
-              <Card Title="영등포 G2존" Paragraph="3.2km" AccentText="" BGAccent={ BGLocationNear } Accent={ NoAccent }></Card>
-              <Card Title="목동 G스타디움" Paragraph="4.8km" AccentText="" BGAccent={ BGLocationMedium } Accent={ NoAccent }></Card>
-              <Card Title="홍대 짱오락실" Paragraph="4.8km" AccentText="" BGAccent={ BGLocationMedium } Accent={ NoAccent }></Card>
+            <div className="Column Group-Card">
+              <Card Title="영등포 G2존" Paragraph="위치" AccentText="3.2km" Accent={ BGLocationNear }></Card>
+              <hr></hr>
+              <Card Title="목동 G스타디움" Paragraph="위치" AccentText="6.3km" Accent={ BGLocationMedium }></Card>
+              <hr></hr>
+              <Card Title="홍대 짱오락실" Paragraph="위치" AccentText="12km" Accent={ BGLocationFar }></Card>
             </div>
           </div>
         </div>
