1fcdf5dbc140bd170c77543bc872f613f8c9d190
heart <66947260+bampudding@users.noreply.github.com>
2023-03-28 05:16:40 +0900

    D

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Settings from './DesignSystem/Component/In-Content/Settings';
 
 function App() {
   return (
-    <div className="System"> { /* 시스템 전반 Flex / Column / Padding / Gap */ }
+    <BrowserRouter basename="https://bampudding.github.io/musescore">
+      <div>
+        <div className="System"> { /* 시스템 전반 Flex / Column / Padding / Gap */ }
 
-      {/* <SearchFind /> */}
-      {/* 차후 Search 관련 function은 전부 위 모듈로 변경예정 */}
-      <Home />
-      {/* <Active/> */}
-      <Settings Icon="" Title="뮤즈파인더 베타"/>
-    </div>
+          {/* <SearchFind /> */}
+          {/* 차후 Search 관련 function은 전부 위 모듈로 변경예정 */}
+          <Home />
+          {/* <Active/> */}
+          <Settings Icon="" Title="뮤즈파인더 베타"/>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
