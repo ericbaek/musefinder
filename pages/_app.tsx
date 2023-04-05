034d559c7719340df4e909274f2a3a78d93def51
heart <66947260+bampudding@users.noreply.github.com>
2023-04-06 00:01:25 +0900

    g

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '@/stories/Import.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>뮤즈파인더</title>
+      <meta name="description" content="오락실을 쉽고 빠르게 검색해보자!" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.svg" />
+      <link rel="apple-touch-icon" href="/favicon_512.png"></link>
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
+
 }
 
-// DO NOT EDIT
+// DO NOT EDIT (META)
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,14 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" className="dark">
-      <Head>
-        <title>뮤즈파인더</title>
-        <meta name="description" content="오락실을 쉽고 빠르게 검색해보자!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
-        <link rel="apple-touch-icon" href="/favicon_512.png"></link>
-      </Head>
+    <Html lang="en">
+      <Head />
       <body>
         <Main />
         <NextScript />
@@ -18,4 +12,4 @@ export default function Document() {
   )
 }
 
-// DO NOT EDIT (META)
+// DO NOT EDIT
