ba4b154f4bc0987f3bc0a446109bf651fd9f4159
heart <66947260+bampudding@users.noreply.github.com>
2023-04-10 17:24:18 +0900

    a

--- modules/serverNear.jsx
+++ modules/serverNear.jsx
@@ -5,7 +5,7 @@ export default function serverNear({latitude, longitude}) {
     console.log("서버니어 실행됐음!");
     return (
         <div>
-            <ContentTitle Title="테스트중입니다." Paragraph="필터"/>
+           <ContentTitle Title='알림' Paragraph=''/>
         </div>
     );
 }
