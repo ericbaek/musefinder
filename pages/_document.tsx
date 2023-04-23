c1df8ca3f0ea411fe498e7768f068713c5e404ac
heart <66947260+bampudding@users.noreply.github.com>
2023-04-23 11:51:36 +0900

    a

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="ko" className="scroll-smooth">
+      
       <Head />
       <body>
         <Main />
         <NextScript />
+        
       </body>
-      <Script src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=jvaprt0m2p" />
+      <script src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=jvaprt0m2p" />
     </Html>
   )
 }
